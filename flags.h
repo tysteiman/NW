6609fec05de81cba0cf33e0ff0fe2cc84b1aa96c
Tyler Steiman <tsteiman@switchboxinc.com>
2017-04-18 18:15:11 -0400

    flags.h change

--- flags.h
+++ flags.h
@@ -1,7 +1,7 @@
 #ifndef NW_FLAGS
 #define NW_FLAGS
 
-int curFlag;              /* Current option flag */
-int terminalFlag = FALSE; /* Terminal flag for no ncurses */
+int curFlag;          /* Current option flag */
+int terminalFlag = 0; /* Terminal flag for no ncurses */
 
 #endif
