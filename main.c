d68ce2b309ff23c4b7999d0de6b98a02b0883264
Tyler Steiman <ma.steiman@gmail.com>
2017-04-19 19:30:33 -0400

    remove docs

--- main.c
+++ main.c
@@ -1,24 +1,3 @@
-/**
- * @todo create new library for file functions. in order to do this
- * we will need to know how structs will be broken down in terms
- * of window, pointer, etc. i'm thinking a window should hold
- * structs like pointer (x, y) and file (name, lines (struct)).
- * Line should hold length, number, before & next pointers
- * assumingly. the back end of that functionality can be completely
- * build in terms of loading the structs with data. after that
- * a testing suite of some sort needs to be built before performing
- * text manipulation operations.
- *
- * @todo we need initialization functions for the structures.
- * the structs can get built off off and returned as needed
- * depending on whether it's a new file or loading a file.
- * simple flags should be able to be passed for that.
- *
- * @todo make library for handling linked lists. we will probably
- * end up with a couple so it's important we can create a library
- * and use the same functions with all linked lists.
- */
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
