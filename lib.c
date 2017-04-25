6351a1fb9172370a0e10f08b3d88a694188553dc
Tyler Steiman <ma.steiman@gmail.com>
2017-04-25 16:03:00 -0400

    freeNodes library function!

--- lib.c
+++ lib.c
@@ -5,35 +5,38 @@
 #include "lib.h"
 
 /**
- * @TODO create library for linked lists. this should probably include
- * freeing nodes for both linked & double linked lists, adding new
- * elements to the list, etc. Maybe create another pointer function
- * for each struct type that can add all the attributes of that
- * struct since they'll all contain different attributes.
+ * Print to stderr `msg` and exit the program execution
+ * with EXIT_FAILURE.
  */
-
 void err(char *msg)
 {
     fprintf(stderr, "ERROR: %s\n", msg);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * Basic integer logging by displaying variable name and value
+ */
 void logint(char *name, int var)
 {
     fprintf(stdout, "VARIABLE -- `%s`: %d -- (int)\n", name, var);
 }
 
+/**
+ * Basic string logging by displaying variable name and value
+ */
 void logstr(char *name, char *str)
 {
     fprintf(stdout, "STRING -- `%s`: %s -- (str)\n", name, str);
 }
 
+/**
+ * Check if file exists and is readable and writable.
+ * Don't allow files that don't have r/w access to be opened for now.
+ * In the future we will probably allow users to open read only files.
+ */
 int fileExists(char *path)
 {
-    /**
-     * Don't allow files that don't have r/w access to be opened for now.
-     * In the future we will probably allow users to open read only files.
-     */
     if (access(path, R_OK|W_OK) != -1)
     {
         return TRUE;
@@ -44,3 +47,27 @@ int fileExists(char *path)
     }
 }
 
+/**
+ * Free up any linked list (single or double) of type hash_t by traversing
+ * node list. This can either be used by nested hash_t structs that have
+ * next/cur pointers or by simply casting structs as a hash_t as long
+ * as they have hash_t *next as a member.
+ * This has been checked with valgrind and should be good to go.
+ */
+void freeNodes(hash_t *head)
+{
+    hash_t *cur;
+    hash_t *next;
+
+    cur = head;
+
+    while (cur->next != NULL)
+    {
+        next = cur->next;
+        free(cur);
+        cur = next;
+    }
+
+    /* When we're at this point we're at the last node */
+    free(cur);
+}
--- lib.h
+++ lib.h
@@ -1,13 +1,23 @@
+/**
+ * lib.h
+ * The main shared library of the NW editor. This is used mainly for global functions
+ * that need to be used throughout the repository. A good example of this is basic
+ * linked list operations like freeNodes, logging, and other helper/shared functions.
+ */
+
 #ifndef NW_LIB
 #define NW_LIB
 
+/* CONSTANTS */
 #define TRUE 1
 #define FALSE 0
 
+/* DECLS */
 void err        (char *msg);
 void logint     (char *name, int var);
 void logstr     (char *name, char *str);
 int  fileExists (char *path);
+void freeNodes  (hash_t *head);
 
 #endif
 
