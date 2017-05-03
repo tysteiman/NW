790dff5dec6771ee5e542ef4b4587c9e39b9e5de
Tyler Steiman <ma.steiman@gmail.com>
2017-05-03 17:39:40 -0400

    Only print up to screen height

--- screen.c
+++ screen.c
@@ -13,11 +13,19 @@ void initScreen(line_t *lines)
     line_t *tmp;
     tmp = lines;
 
-    while (tmp != NULL)
+    while (tmp != NULL && tmp->number != screen.height)
         {
             printw("%s", tmp->content);
             tmp = tmp->next;
         }
+
+    /**
+     * @TODO this end tag serves as a placeholder for where
+     * our status bar can go! that will only need to be one line
+     * and can hopefully be swapped out without doing a bunch
+     * of horse trash on the side.
+     */
+    printw("<end>\n");
 
     move(0, 0);
     refresh();
