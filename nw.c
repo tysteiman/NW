40731d2bd94b7dd4fe95ca07487129d75c768ee9
Tyler Steiman <ma.steiman@gmail.com>
2017-05-04 18:57:07 -0400

    Still use curses when argv == 1

--- nw.c
+++ nw.c
@@ -6,6 +6,8 @@
 
 int main(int argc, char **argv)
 {
+    char *fileName;
+
     if (argc > 1)
         {
             /**
@@ -15,48 +17,49 @@ int main(int argc, char **argv)
              * ready to go.
              */
             parseOpts(argc, argv);
-
-            file_t file;
-
-            /**
-             * Initialize the file based on filename. When the
-             * file we're targeting doesn't exist we still get back
-             * a built out line_t object with a single line.
-             */
-            loadFile(&file, opts.fileName);
-
-            if (opts.debug)
-                {
-                    dumpFile(file);
-                }
-            else
-                {
-                    initScreen(file.lines);
-                }
-
-            /**
-             * @TODO make this work with our freeNodes function
-             */
-            line_t *cur;
-            line_t *next;
-
-            cur = file.lines;
-
-            /**
-             * Free up our line_t nodes from memory. This needs
-             * to be a little more agnostic and moved into a
-             * good function that can clear any nodes passed
-             * instead.
-             */
-            while (cur->next != NULL)
-                {
-                    next = cur->next;
-                    free(cur);
-                    cur = next;
-                }
-
-            free(cur);
+            fileName = opts.fileName;
         }
+
+    file_t file;
+
+    /**
+     * Initialize the file based on filename. When the
+     * file we're targeting doesn't exist we still get back
+     * a built out line_t object with a single line.
+     */
+    loadFile(&file, opts.fileName);
+
+    if (opts.debug)
+        {
+            dumpFile(file);
+        }
+    else
+        {
+            initScreen(file.lines);
+        }
+
+    /**
+     * @TODO make this work with our freeNodes function
+     */
+    line_t *cur;
+    line_t *next;
+
+    cur = file.lines;
+
+    /**
+     * Free up our line_t nodes from memory. This needs
+     * to be a little more agnostic and moved into a
+     * good function that can clear any nodes passed
+     * instead.
+     */
+    while (cur->next != NULL)
+        {
+            next = cur->next;
+            free(cur);
+            cur = next;
+        }
+
+    free(cur);
 
     return 0;
 }
