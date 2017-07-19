2d276edf1e3ddd8c4c0bac8c3670a43aecb5b1c4
Tyler Steiman <tsteiman@switchboxinc.com>
2017-07-19 11:56:14 -0400

    add test documentation

--- test/test.c
+++ test/test.c
@@ -4,6 +4,15 @@
 #include "../file.h"
 #include "../lib.h"
 
+/**
+ * Asserts `result' is true and if not displaying message with
+ * file information. This is a 'simple' assertion meaning it just
+ * makes sure we are at the point we want to be. We probably need
+ * more information than this in reality in terms of displaying
+ * the actual values? Maybe also pass the 'thing' we're checking
+ * the value of (assert file->cursor.x == 3 by passing file->cursor.x
+ * to the function as an argument itself).
+ */
 void 
 nw_assert(int result, char *msg, char *file)
 {
