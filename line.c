5788af5e942ba38dd31a8c1162a7e2c3388d38d3
Tyler Steiman <tsteiman@switchboxinc.com>
2017-07-25 08:47:24 -0400

    deleteChar (and test) works!

--- line.c
+++ line.c
@@ -24,4 +24,35 @@ char * substr(int start, int end, char *str)
 
 void deleteChar(char *str, int index)
 {
+    char *beg = substr(0, (index - 1), str);
+    char *end = substr((index + 1), strlen(str), str);
+
+    int len = strlen(beg) + strlen(end);
+
+    char newStr[len];
+
+    int count = 0;
+    int iBeg = 0;
+    int iEnd = 0;
+
+    for (; iBeg < strlen(beg); iBeg++)
+        {
+            newStr[count] = beg[iBeg];
+            count++;
+        }
+
+    for (; iEnd < strlen(end); iEnd++)
+        {
+            newStr[count] = end[iEnd];
+            count++;
+        }
+
+    newStr[count] = '\0';
+
+    /**
+     * @TODO we should probably make this into a helper function for
+     *       erasing & replacing strings!
+     */
+    bzero(str, strlen(str));
+    strcpy(str, newStr);
 }
--- test/test-line.c
+++ test/test-line.c
@@ -3,12 +3,14 @@
 
 void deleteCharTest(file_t *file)
 {
-    char * name = "Tyler";
+    char nameAr[MAX_LINE_LENGTH] = "Tyler";
+
+    char *name = &nameAr[0];
 
     deleteChar(name, 2);
 
     nw_assert_string(name, "Tyer", "Deleting index 2 from Tyler results in Tyer",
-            __FILE__, __LINE__, (char *)__FUNCTION__, file);
+                     __FILE__, __LINE__, (char *)__FUNCTION__, file);
 }
 
 void substrTest(file_t *file)
@@ -17,7 +19,7 @@ void substrTest(file_t *file)
 
     char * res = substr(0, 3, str);
     nw_assert_string(res, "Scuz", "Substring 0-3 of Scuzbucket is Scuz",
-            __FILE__, __LINE__, (char *)__FUNCTION__, file);
+                     __FILE__, __LINE__, (char *)__FUNCTION__, file);
 
     char * end = substr(4, 9, str);
     nw_assert_string(end, "bucket", "Substring 4-9 of Scuzbucket is bucket",
