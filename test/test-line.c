5f4e264de2af108c13b7c780c1159e55479c1902
Tyler Steiman <tsteiman@switchboxinc.com>
2017-07-25 09:02:05 -0400

    add tests for deleteChar from beginning & end of string

--- test/test-line.c
+++ test/test-line.c
@@ -3,14 +3,29 @@
 
 void deleteCharTest(file_t *file)
 {
+    /* first test (clip from middle) */
     char nameAr[MAX_LINE_LENGTH] = "Tyler";
-
     char *name = &nameAr[0];
-
     deleteChar(name, 2);
 
     nw_assert_string(name, "Tyer", "Deleting index 2 from Tyler results in Tyer",
-                     __FILE__, __LINE__, (char *)__FUNCTION__, file);
+		     __FILE__, __LINE__, (char *)__FUNCTION__, file);
+
+    /* second test (clip from end) */
+    char secondNameAr[MAX_LINE_LENGTH] = "Tyler";
+    char *secondName = &secondNameAr[0];
+    deleteChar(secondName, 4);
+
+    nw_assert_string(secondName, "Tyle", "Deleting from end of Tyler results in Tyle",
+		     __FILE__, __LINE__, (char *)__FUNCTION__, file);
+
+    /* third test (clip from beginning) */
+    char thirdNameAr[MAX_LINE_LENGTH] = "Tyler";
+    char *thirdName = &thirdNameAr[0];
+    deleteChar(thirdName, 0);
+
+    nw_assert_string(thirdName, "yler", "Deleting first index from Tyler results in yler",
+		     __FILE__, __LINE__, (char *)__FUNCTION__, file);
 }
 
 void substrTest(file_t *file)
@@ -19,9 +34,9 @@ void substrTest(file_t *file)
 
     char * res = substr(0, 3, str);
     nw_assert_string(res, "Scuz", "Substring 0-3 of Scuzbucket is Scuz",
-                     __FILE__, __LINE__, (char *)__FUNCTION__, file);
+		     __FILE__, __LINE__, (char *)__FUNCTION__, file);
 
     char * end = substr(4, 9, str);
     nw_assert_string(end, "bucket", "Substring 4-9 of Scuzbucket is bucket",
-                     __FILE__, __LINE__, (char *)__FUNCTION__, file);
+		     __FILE__, __LINE__, (char *)__FUNCTION__, file);
 }
