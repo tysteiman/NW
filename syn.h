0ca19687029ce4b306f99fc49246b90cd7611dbc
Tyler Steiman <tsteiman@switchboxinc.com>
2017-08-08 09:39:28 -0400

    Add license

--- syn.h
+++ syn.h
@@ -1,4 +1,22 @@
 /**
+ *   NW Editor - Super light terminal based editor
+ *   Copyright (C) 2017 Tyler Steiman
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * syn.h
  *
  * Header file for syn.c (syntax libary)
